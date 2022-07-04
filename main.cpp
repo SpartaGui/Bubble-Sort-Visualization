d2ee3a52947d668bb35f99ffbfcdd6ea8192b4dc
Sparta <76192542+SpartaGui@users.noreply.github.com>
2022-07-04 17:08:12 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -29,46 +29,47 @@ void gotoxy(int x, int y){
 
 int main(){
 	srand(time(NULL));
-    system("cls");
-    system("pause");
-    system("cls");
+	system("cls");
+	system("pause");
+	system("cls");
+	
 	for(int i = 0;i<tamanho;i++){
 		num[i] = rand()%tamanho+1;
 	}
 	int feitos = tamanho-1;
 
 	while(feitos != 0){
-	for(int i = 0;i<feitos;i++){
-		//renderizar
-		string tela = "";
-		gotoxy(0, 0);
-		for(int j = 0;j<tamanho;j++){
-			if(j == i){
-				cout<<tela;
-				tela = "";
+		for(int i = 0;i<feitos;i++){
+			//renderizar
+			string tela = "";
+			gotoxy(0, 0);
+			for(int j = 0;j<tamanho;j++){
+				if(j == i){
+					cout<<tela;
+					tela = "";
+				}
+				for(int k = 0;k<tamanho;k++){
+					if(k<num[j])
+					tela+="#";
+					else
+					tela+=" ";
+				}
+				tela+="\n";
+				if(j == i){
+					textcolor(10);
+					cout<<tela;
+					tela = "";
+					textcolor(7);
+				}
 			}
-			for(int k = 0;k<tamanho;k++){
-                if(k<num[j])
-                tela+="#";
-                else
-                tela+=" ";
-			}
-			tela+="\n";
-			if(j == i){
-                textcolor(10);
-				cout<<tela;
-				tela = "";
-				textcolor(7);
+			cout<<tela;
+			//swap
+			if(num[i]>num[i+1]){
+				int temp = num[i];
+				num[i] = num[i+1];
+				num[i+1] = temp;
 			}
 		}
-		cout<<tela;
-		//swap
-		if(num[i]>num[i+1]){
-			int temp = num[i];
-			num[i] = num[i+1];
-			num[i+1] = temp;
-		}
-	}
 	feitos--;
 	}
 
